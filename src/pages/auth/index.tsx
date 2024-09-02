9a261244077671b4d6faeef7788436c63a1779e7
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-09-02 07:31:07 +0700

    Make Front-end for this project

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,20 @@
+import { SignedOut, SignInButton, SignUpButton, SignedIn } from "@clerk/clerk-react"
+import { Navigate } from "react-router-dom"
+
+export const Auth = () => {
+
+    return (
+        <div className="sign-in-container">
+            <SignedOut>
+                <SignUpButton mode="modal"/>
+                <SignInButton mode="modal"/>
+            </SignedOut>
+            <SignedIn>
+                <Navigate to="/"/>
+            </SignedIn>
+
+        </div>
+
+
+    );
+}
